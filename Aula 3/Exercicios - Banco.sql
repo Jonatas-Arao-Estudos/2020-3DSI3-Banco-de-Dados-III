390fd061485d17d94465d3498d5b2bb996128771
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-03-10 21:37:39 -0300

    Aula 3 - Stay in Standing By

--- Aula 3/Exercicios - Banco.sql
+++ Aula 3/Exercicios - Banco.sql
@@ -71,7 +71,8 @@ SELECT
     MI.nm_produto 'Nome',
     MI.nm_fabricante 'Fabricante',
     MI.ds_produto 'Descrição',
-    MI.vl_produto 'Valor'
+    MI.vl_produto 'Valor Valor - Mega Informática',
+    PI.vl_produto 'Valor - Pikachu Informática'
 FROM
 	estoque_megainformatica MI
     INNER JOIN
@@ -84,7 +85,8 @@ SELECT
     LE.nm_produto 'Nome',
     LE.nm_fabricante 'Fabricante',
     LE.ds_produto 'Descrição',
-    LE.vl_produto 'Valor'
+    LE.vl_produto 'Valor - Loja que Explode',
+    MI.vl_produto 'Valor - Mega Informática'
 FROM
 	estoque_lojaqueexplode LE
     INNER JOIN
@@ -92,20 +94,127 @@ FROM
 ;
 
 /* 7. Quais itens aparecem no estoque da pikachu informática e não aparecem no estoque da mega informática? */
+SELECT
+	PI.id_produto 'ID',
+    PI.nm_produto 'Nome',
+    PI.nm_fabricante 'Fabricante',
+    PI.ds_produto 'Descrição',
+    PI.vl_produto 'Valor - Pikachu Informática'
+FROM
+	estoque_pikachuinformatica PI
+    LEFT JOIN
+    estoque_megainformatica MI ON (PI.id_produto = MI.id_produto)
+WHERE
+	MI.id_produto is null
+;
 
 /* 8. Liste todos os itens da pikachu informática em ordem alfabética. */
+SELECT
+	id_produto 'ID',
+    nm_produto 'Nome',
+    nm_fabricante 'Fabricante',
+    ds_produto 'Descrição',
+    vl_produto 'Valor'
+FROM
+	estoque_pikachuinformatica
+ORDER BY(nm_produto) ASC
+;
 
 /* 9. Liste todos os itens da mega informática em ordem decrescente de preço. */
+SELECT
+	id_produto 'ID',
+    nm_produto 'Nome',
+    nm_fabricante 'Fabricante',
+    ds_produto 'Descrição',
+    vl_produto 'Valor'
+FROM
+	estoque_megainformatica
+ORDER BY(vl_produto) DESC
+;
 
 /* 10. Liste todos os itens da pikachu informática em ordem crescente de preço excluindo os itens que custem menos de 600,00 e mais de 4000,00. */
+SELECT
+	id_produto 'ID',
+    nm_produto 'Nome',
+    nm_fabricante 'Fabricante',
+    ds_produto 'Descrição',
+    vl_produto 'Valor'
+FROM
+	estoque_pikachuinformatica
+WHERE
+	vl_produto > 600 AND vl_produto < 4000
+ORDER BY(vl_produto) ASC
+;
 
 /* 11. Liste todos os itens da super info e da loja que explode que custem mais de 500,00 e menos de 1000,00. */
+SELECT
+	SI.id_produto 'ID',
+    SI.nm_produto 'Nome',
+    SI.nm_fabricante 'Fabricante',
+    SI.ds_produto 'Descrição',
+    SI.vl_produto 'Valor - Loja Super Info',
+    LE.vl_produto 'Valor - Loja que Explode'
+FROM
+	estoque_superinfo SI
+    LEFT JOIN
+    estoque_lojaqueexplode LE ON(SI.id_produto = LE.id_produto)
+WHERE
+	(SI.vl_produto > 500 AND SI.vl_produto < 1000) OR (LE.vl_produto > 500 AND LE.vl_produto < 1000)
+UNION
+SELECT
+	LE.id_produto 'ID',
+    LE.nm_produto 'Nome',
+    LE.nm_fabricante 'Fabricante',
+    LE.ds_produto 'Descrição',
+    SI.vl_produto 'Valor - Loja Super Info',
+    LE.vl_produto 'Valor - Loja que Explode'
+FROM
+	estoque_superinfo SI
+    RIGHT JOIN
+    estoque_lojaqueexplode LE ON(SI.id_produto = LE.id_produto)
+WHERE
+	SI.id_produto is null AND (SI.vl_produto > 500 AND SI.vl_produto < 1000) OR (LE.vl_produto > 500 AND LE.vl_produto < 1000)
+;
 
 /* 12. Liste todos os itens da pikachu informática que sejam da marca “HP”. */
+SELECT
+	id_produto 'ID',
+    nm_produto 'Nome',
+    nm_fabricante 'Fabricante',
+    ds_produto 'Descrição',
+    vl_produto 'Valor'
+FROM
+	estoque_pikachuinformatica
+WHERE
+	nm_fabricante = "HP"
+;
 
 /* 13. Liste todos os itens da loja que explode que em sua descrição contem com a palavra “Ryzen”. */
+SELECT
+	id_produto 'ID',
+    nm_produto 'Nome',
+    nm_fabricante 'Fabricante',
+    ds_produto 'Descrição',
+    vl_produto 'Valor'
+FROM
+	estoque_lojaqueexplode
+WHERE
+	ds_produto LIKE "%Ryzen%"
+;
 
 /* 14. Liste todos os itens da loja super info que possuam a letra “f” em seu nome e que custem menos de 700,00. */
+SELECT
+	id_produto 'ID',
+    nm_produto 'Nome',
+    nm_fabricante 'Fabricante',
+    ds_produto 'Descrição',
+    vl_produto 'Valor'
+FROM
+	estoque_superinfo
+WHERE
+	nm_produto LIKE "%f%" AND
+    vl_produto < 700
+;
 
 /* 15. Qual a média de valores dos itens da marca AMD na loja pikachu informática? */
 
